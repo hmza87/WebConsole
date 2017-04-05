eedbf9cba54e2f5c959c2c5a5e67f16eaa28cde7
Lionz Hunter <hshowmail@gmail.com>
2017-04-05 15:18:05 +0000

    core changes, see change log

---
+++ assets/js/lib.js
@@ -0,0 +1,82 @@
+var pick=true;
+(function($){
+    var c = $('#console-body input');
+    var mouseIn=false, c_x, c_y;
+    
+    c.on('keyup', function(e){
+        if(e.keyCode==13){
+            Ev(c.val());
+
+
+            $('#console-history').append(c.val());
+            c.val('$(')
+        }
+    });
+    
+
+    
+    
+
+})(jQuery)
+
+jQuery(document).ready(function($){
+    $('#console').draggable({handle: '#console-head'})
+                    .resizable()
+                    .click(function(){
+                        $('#console-body input').focus();
+                    });
+
+    
+    $(document).mousemove(function(e){
+        if(pick) selectElemnt($(e.target));
+    }).mouseleave(function(e){
+        //$(e.target).removeClass('hzc-selected');
+    }); 
+    $('*').mouseenter(function(){
+        if(pick){
+            $(this).css('margin', parseInt($(this).css('margin')-1))
+        }
+    }).mouseleave(function(){
+        if(pick){
+            $(this).css('margin', parseInt($(this).css('margin')+1))
+        }
+    });
+
+    
+});
+
+
+var Ev = function(s){
+    try {
+        eval(s);
+    }
+    catch(err) {
+        $('#console-history').append(err);
+    }
+}
+function selectElemnt(dis){
+     $('.hzc-selected').removeClass('hzc-selected');
+     
+
+     console.log(dis.localName);
+
+     
+         dis.addClass('hzc-selected')
+         
+         .mouseleave(function(){
+            $(this).removeClass('hzc-selected').css('margin',dis.css('margin')+ 1);
+            $('#overlay').hide();
+       });
+       $('.hzc-selected').click(handleEclick);
+}
+
+function handleEclick( event ) {
+        event.preventDefault();
+        var selector = (typeof($(this).attr('id')) !== 'undefined' || $(this).attr('id') !== null) ? '#' + $(this).attr('id') :  '.' + $(this).attr('class');
+        console.log($(this).myPlugin)
+            
+}
+
+jQuery.fn.getSelector = function() {
+        return this.data('selector');
+};
